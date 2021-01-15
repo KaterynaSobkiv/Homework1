1f21644ce9ad7a33b59d69b2323ac0fc1b45d01a
KaterynaSobkiv <kateryna.sobkiv@sombrainc.com>
2021-01-15 15:54:19 +0200

    Homework5

--- Homework5/Student.java
+++ Homework5/Student.java
@@ -1,2 +1,29 @@
-package Homework5;public class Student {
+package Homework5;
+
+
+public class Student {
+    public String sName = "";
+    public int iGrade = 0;
+    public int iAge = 0;
+
+    public String getsName() {
+        return sName;
+    }
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+    public int getiGrade() {
+        return iGrade;
+    }
+    public void setiGrade(int iGrade) {
+        this.iGrade = iGrade;
+    }
+    public int getiAge() {
+        return iAge;
+    }
+    public void setiAge(int iAge) {
+        this.iAge = iAge;
+    }
+
 }
+
--- Homework5/StudentComparator.java
+++ Homework5/StudentComparator.java
@@ -1,2 +1,12 @@
-package Homework5;public class StudentComparator {
+package Homework5;
+
+
+public class StudentComparator implements Comparable<Student>{
+
+    @Override
+    public int compareTo(Student s){
+        return s.sName.compareToIgnoreCase(s.sName);//"z-a" s.sName.compareToIgnoreCase(sName)
+    }
+
+
 }
